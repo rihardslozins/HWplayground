21c0f6dbc0a1ac3c904dff78850875faaa5c637e
rihardslozins <rihards.lozins@gmail.com>
2021-01-29 11:42:05 +0200

    Homework1

--- Excercise2.playground/Contents.swift
+++ Excercise2.playground/Contents.swift
@@ -6,7 +6,9 @@ import UIKit
 Create a constant named myAge and assign it the value of your age.
 */
 
+
 let myAge = 31
+
 
 //MARK: - Exercise 2.2
 
@@ -15,9 +17,11 @@ let myAge = 31
  after 10 years(use the constant myAge and the addition(+) operator).
 */
 
+
 let a = 10
 
 var myAgeInTenYears = myAge + a
+
 
 //MARK: - Exercise 2.3
 
@@ -25,7 +29,9 @@ var myAgeInTenYears = myAge + a
 Declare another daysInYear constant and set it to 365.25 (the number of days in a leap year).
 */
 
+
 let daysInYear = 365.25
+
 
 //MARK: - Exercise 2.4
 
@@ -33,14 +39,17 @@ let daysInYear = 365.25
 Declare a Float variable named daysPassed and set it to the number of days since you were born plus 10 years (use myAgeInTenYears(variable) and daysInYear(constant)).
 */
 
+
 var tenYears = Float(daysInYear) * 10
 
 var daysPassed = Float(myAge) * tenYears
+
 
 //MARK: - Exercise 2.5
 
 /*
 Using the print() function, print the phrases like: "My age is <...> years. In 10 years, I will be <...> years old. From the moment of my birth day have passed <...>" . (Symbols (<...>) must be replaced with variable values !)
 */
+
 
 print("My age is \(myAge) years. In 10 years, I will be \(myAgeInTenYears) years old. From the moment of my birth day, have passed \(daysPassed)")
